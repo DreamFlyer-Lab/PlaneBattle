dbb74c5ab982b177f641f9a83b30a5b40d483564
飞扬 理想 <lixiang985211@hotmail.com>
2020-05-23 11:22:09 +0800

    修复bug之后的版本

--- PlaneBattle.cpp
+++ PlaneBattle.cpp
@@ -16,25 +16,28 @@ using namespace std;
 void hello();
 class area
 {
-public:
+private:
 	char uarea[H][W] = {' '};//uarea:构成整个游戏界面的二维字符数组，H为横坐标，W为纵坐标
 	int bullet[W-4] = {-1};//bullet:存储子弹的横坐标，去掉边框和飞机占用的两格总共W-4
-	int score = 0;//score:分数
-	int enemy[ENEMYNUM][2] = {-1 };//enemy:敌人的编号及坐标，0是H坐标，Y是W坐标
 	int gamespeed = 500;//gamespeed:刷新速度
 	void firstbuild();//firstbuild():向uarea中写入边框
 	void setplane(int x, int y,bool appear);//setplane：根据给定的坐标写入或删除玩家飞机x:横坐标y:纵坐标appear:删除还是新增
 	void setenemy(int x, int y,bool appear);//setenemy:根据给定的坐标写入或删除敌人x:横坐标y:纵坐标appear:删除还是新增
 	void moveplane(int &x, int &y, int forward);//moveplane :根据给定的坐标和方向写入新飞机，删除旧飞机。同时改变坐标。x:横坐标y:纵坐标forward:方向
 	void setbullet(int x, int y);//;seybullet:根据给定的飞机位置进行子弹的移动及新增x:横坐标y:纵坐标
-	void rungame(int &x,int &y,int enemy[][2],int &score);//rungame:游戏进行的主程序，调用其他函数完成一个循环x:横坐标y:纵坐标enemy:敌人数组score:分数
 	void newenemy(int enemy[][2]);//newenemy:在界面数组的最上方生成新的敌方飞机enemy:敌人数组
 	void enemymove(int enemy[][2]);//enemymove:在界面数组中清楚敌人的旧位置，同时在enemy数组中更新敌人的位置
 	int controlenemydead(int bullet[W],int enemy[][2]);//controlenemydead:根据给定的子弹数组和敌人位置数组判定敌人是否死亡，同时在界面中打印敌人的位置，更新分数
 	bool controlplanedead(int x, int y, int enemy[][2]);//controlplanedead:判定自己的飞机是否死亡x:横坐标y:纵坐标
 	void gameover();//gameover:结束游戏并显示分数
 	void printarea();//printarea:打印界面数组
+public:
+	int score ;//score:分数
+	int enemy[ENEMYNUM][2];//enemy:敌人的编号及坐标，0是H坐标，Y是W坐标
 	area();
+	void rungame(int& x, int& y, int enemy[][2], int& score);//rungame:游戏进行的主程序，调用其他函数完成一个循环x:横坐标y:纵坐标enemy:敌人数组score:分数
+	void testwrite(int x,int y);
+	int round;
 };
 void area::firstbuild()
 {
@@ -66,7 +69,7 @@ void area::setenemy(int x, int y,bool appear)
 	if (appear == true)
 	{
 		uarea[x][y] = ENEMYCHAR;
-		if (x != 1 && x != H - 2 && y != 1 && y != W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
+		if (x > 1 && x < H - 2 && y >1 && y < W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
 		{
 			uarea[x - 1][y] =ENEMYCHAR;
 			uarea[x + 1][y] = ENEMYCHAR;
@@ -76,7 +79,7 @@ void area::setenemy(int x, int y,bool appear)
 	if (appear == false)
 	{
 		uarea[x][y] = ' ';
-		if (x != 1 && x != H - 2 && y != 1 && y != W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
+		if (x > 1 && x < H - 2 && y >1 && y < W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
 		{
 			uarea[x - 1][y] = ' ';
 			uarea[x + 1][y] = ' ';
@@ -91,7 +94,7 @@ void area::setplane(int x, int y,bool appear)
 	if (appear == true) 
 	{
 		uarea[x][y] = PLANECHAR;
-		if (x != 1 && x != H - 2 && y != 1 && y != W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
+		if (x >1 && x < H - 2 && y >1 && y< W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
 		{
 			uarea[x - 1][y] = PLANECHAR;
 			uarea[x + 1][y] = PLANECHAR;
@@ -101,7 +104,7 @@ void area::setplane(int x, int y,bool appear)
 	if (appear == false)
 	{
 		uarea[x][y] = ' ';
-		if (x != 1 && x != H - 2 && y != 1 && y != W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
+		if (x >1 && x < H - 2 && y> 1 && y < W - 2)//如果碰到边框就不显示飞机形状，只显示一个点
 		{
 			uarea[x - 1][y] =' ';
 			uarea[x + 1][y] = ' ';
@@ -112,25 +115,25 @@ void area::setplane(int x, int y,bool appear)
 }
 void area::moveplane(int &x, int &y, int forward)
 {
-	if (forward == 0&&x-1!=0)//左
+	if (forward == 0&&x-1>0)//左
 	{
 		setplane(x, y, false);
 		setplane(x - 1, y, true);
 		x = x - 1;
 	}
-	if (forward == 1&&x+1!=H-1)//右
+	if (forward == 1&&x+1<H-1)//右
 	{
 		setplane(x, y, false);
 		setplane(x + 1, y, true);
 		x = x + 1;
 	}
-	if (forward == 2&&y-1!=2)//上
+	if (forward == 2&&y-1>2)//上
 	{
 		setplane(x, y, false);
 		setplane(x, y - 1, true);
 		y = y - 1;
 	}
-	if (forward == 3&&y+1!=W-1)//下
+	if (forward == 3&&y+1<W-1)//下
 	{
 		setplane(x, y, false);
 		setplane(x , y+1, true);
@@ -150,11 +153,22 @@ void area::setbullet(int x, int y)
 		}
 
 	}
+	
 	for (int i = 0; i <y-3; i++)
 	{
 		bullet[i] = bullet[i + 1];//子弹的移动
 	}
+	//for (int i = 0; i < W - 4; i++)
+	//{
+	//	cout << "test break1 "<<i<<"bullet"<< bullet[i] << endl;
+	//}
+	//Sleep(1000);
 	bullet[y - 3] = x;//新增子弹
+	/*for (int i = 0; i < W - 4; i++)
+	{
+		cout << "test break2" << i << "bullet"<<bullet[i] << endl;
+	}*/
+	//Sleep(1000);
 	for (int i = 0; i < W-4; i++)
 	{
 		int t = bullet[i];
@@ -235,9 +249,18 @@ void area::enemymove(int enemy[][2])
 		}
 
 	}
+	
 	for (int i = 0; i < ENEMYNUM; i++)
 	{
-		enemy[i][1]++;
+		if (enemy[i][1] == 20)
+		{
+			enemy[i][0] = -1;
+			enemy[i][1] = -1;
+		}
+		else
+		{
+			enemy[i][1]++;
+		}
 	}
 }
 void area::gameover()
@@ -275,12 +298,13 @@ void area::rungame(int &x,int &y,int enemy[][2],int &score)
 	case 'q':gameover(); break;
 	default:break;
 	}
-
-	setbullet(x, y);
+	int x_ = x; int y_ = y;
+	setbullet(x_, y_);
 	enemymove(enemy);
 	int thisscore=controlenemydead(bullet, enemy);
-	bool dead=controlplanedead(x, y, enemy);
+	bool dead=controlplanedead(x_, y_, enemy);
 	printarea();
+	testwrite(x, y);
 	if (dead == true)
 	{
 		gameover();
@@ -324,7 +348,8 @@ area::area()
 		enemy[i][1] = -1;
 	}
 	gamespeed = 500;
-	
+	score = 0;
+	round = 0;
 	//newenemy(enemy);
 	//setbullet(15, 18);
 	//controlenemydead(bullet, enemy);
@@ -360,8 +385,17 @@ int main()
 	int x1 = H / 2;
 	int y1 = W - 2;
 	area t;
+	/*while (x1 != 200)
+	{
+		cin >> x1 >> y1;
+		t.setbullet(x1, y1);
+		for (int i = 0; i < W - 4; i++)
+		{
+			cout << "test break" << t.bullet[i] << endl;
+		}
+	}*/
 	//int test = 0;
-	while (true)
+    while (true)
 	{
 		/*ofstream out;
 		out.open("testbreak.log");
@@ -393,6 +427,43 @@ void hello()
 		if (i > 0)
 			cout << "\n\n\t\t倒计时：" << i << endl;
 	}
+}
+void area::testwrite(int x,int y)
+{
+	fstream fs;
+	fs.open("logger.txt", ios::app);
+	if (fs.good())
+	{
+		fs << "the " << round << "round"<<endl;
+		fs << "the plane" << "X axis   " << x << endl;
+		fs << "the plane" << "Y axis   " << y << endl;
+		fs << "the map:" << endl;
+		for (int i = 0; i < W; i++)
+		{
+			for (int j = 0; j < H; j++)
+			{
+				fs << uarea[j][i];
+			}
+			if (i == 5)
+			{
+				fs << "当前分数：" << score;
+			}
+			fs <<endl;
+		}
+		fs << "the bullet" << endl;
+		for (int i = 0; i < W - 4; i++)
+			fs << i << "    " << bullet[i] << "    " << endl;
+		fs << "the enemy" << endl;
+		for (int i = 0; i < ENEMYNUM; i++)
+		{
+			fs << i << "x axis   " << enemy[i][0]<<endl;
+			fs << i << "y axis    " << enemy[i][1]<<endl;
+
+		}
+
+	}
+	fs.close();
+	
 }
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
